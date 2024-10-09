f295d504025cea86663375bfe242bd766fdc8fc6
vevekita <108936525+vevekita@users.noreply.github.com>
2024-10-09 08:44:49 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,7 +2,28 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function buscarInformacoes(){
     const res = await fetch(url);
-    const dados = res.json(dados);
+    const dados = await res.json();
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container_texto');
+
+    const pessoasConectadas = dados.total_pessoas_conectadas / 1e9;
+    const pessoasNoMundo = dados.total_pessoas_mundo / 1e9;
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round(dados.tempo_medio - horas)*60;
+    const porcentagemConectada = (pessoasConectadas/pessoasNoMundo)*100;
+    paragrafo.innerHTML = `Você sabia que o mundo tem 
+    <span>${pessoasNoMundo}</span> 
+    bilhões de pessoas e que aproximadamente 
+    <span>${pessoasConectadas}</span> 
+    bilhões estão conectadas em alguma rede social e passam em média 
+    <span>${horas}</span> 
+    horas e 
+    <span>${minutos}</span>
+    minutos conectadas.
+    Isso significa que aproximadamente 
+    <span>${porcentagemConectada}%</span>
+    de pessoas que estão conectadas em alguma rede social. `
+    document.getElementById('graficos-container').appendChild(paragrafo);
 }
 
 buscarInformacoes();
